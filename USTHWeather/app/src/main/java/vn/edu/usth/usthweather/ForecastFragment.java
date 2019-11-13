348ff57f7caa2b4da0f1152c88737ec8e7fb9318
Thanh <thanhnt.b8170@st.usth.edu.vn>
2019-11-13 11:11:52 +0700

    Commit practical 3&4

---
+++ USTHWeather/app/src/main/java/vn/edu/usth/usthweather/ForecastFragment.java
@@ -0,0 +1,36 @@
+package vn.edu.usth.usthweather;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ForecastFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ForecastFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ForecastFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_forecast, container, false);
+//        View v = new View(getContext());
+//        v.setBackgroundColor(0xFF800080);
+//        return v;
+    }
+
+}
