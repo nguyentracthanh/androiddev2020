6e3d0575001a8ef3a107d4e102afe7a0a1a056f5
Thanh <thanhnt.b8170@st.usth.edu.vn>
2019-12-12 02:26:51 +0700

    sorry, I forgot commit labwork 13 before, here are 13 and 14

--- Weather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ Weather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -6,17 +6,31 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 import com.google.android.material.tabs.TabLayout;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class WeatherActivity extends AppCompatActivity {
@@ -68,6 +82,68 @@ public class WeatherActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.refesh:
+                Toast.makeText(this,"Refresh",Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.setting:
+                Toast.makeText(this,"Setting",Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private class GetRequestImage extends AsyncTask<String, Integer, Bitmap> {
+        private String resp;
+        ProgressDialog progressDialog;
+        Bitmap bitmap;
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            try {
+                URL url = new URL(params[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream is = connection.getInputStream();
+                bitmap = BitmapFactory.decodeStream(is);
+                return bitmap;
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+
+        }
+
+        protected void onPostExecute(Bitmap bitmap) {
+            // execution of result of Long time consuming operation
+            super.onPostExecute(bitmap);
+            progressDialog.dismiss();
+            LinearLayout linearLayout = (LinearLayout) findViewById(R.id.fragment1);
+            Drawable bitmapDrawable = new BitmapDrawable(getResources(), bitmap);
+            linearLayout.setBackgroundDrawable(bitmapDrawable);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            progressDialog = ProgressDialog.show(WeatherActivity.this,
+                    "Updating weather...",
+                    "Wait for 5 seconds!");
+        }
+
+
+        protected void onProgressUpdate(String... text) {
+            // Do something here
+        }
     }
 
     @Override
